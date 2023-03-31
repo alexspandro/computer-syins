054c3759261e14c3c3433c154fd3b1092e419a0d
alexspandro <129217632+alexspandro@users.noreply.github.com>
2023-03-31 15:37:05 -0400

    Update CustomerSystem.py

--- CustomerSystem.py
+++ CustomerSystem.py
@@ -82,7 +82,7 @@ def generateCustomerDataFile():
   
   fileName = input("Generating new customer file...\nWhat do you want to name this file?")
   folderName = input("Which existing folder would you like to assign the file to?")
-  fileCreateNew = folder + "\\(folderName)\\(fileName).txt"
+  fileCreateNew = folder + "\\(folderName)\\(fileName).csv"
   file = open("fileName", "w")
   file.writelines("NEW CUSTOMER INFO IN FOLDER")
   file.writelines("First Name:", CustFirstName)
